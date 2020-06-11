2dd153bda2da5017d527f6eb35ef626f26363b43
MangKyu <whalsrb1226@naver.com>
2020-06-11 18:37:40 +0900

    Add configurations

--- src/main/java/com/mang/example/security/SecurityApplication.java
+++ src/main/java/com/mang/example/security/SecurityApplication.java
@@ -2,9 +2,16 @@ package com.mang.example.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(SecurityApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
