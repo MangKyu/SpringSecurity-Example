8afa064c955757a592f2ef3fb1aa66098cc0d325
MangKyu <whalsrb1226@naver.com>
2020-06-12 12:04:42 +0900

    Add authentication module

---
+++ src/main/java/com/mang/example/security/config/security/CustomAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.mang.example.security.config.security;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Log4j2
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super.setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(request.getParameter("userEmail"), request.getParameter("userPw"));
+        setDetails(request, authRequest);
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+
+}
---
+++ src/main/java/com/mang/example/security/config/security/CustomAuthenticationProvider.java
@@ -0,0 +1,47 @@
+package com.mang.example.security.config.security;
+
+import com.mang.example.security.app.user.model.UserDetailsVO;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.annotation.Resource;
+
+@RequiredArgsConstructor
+@Log4j2
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Resource(name = "userDetailsService")
+    private UserDetailsService userDetailsService;
+    @NonNull
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        log.error("##############");
+        UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) authentication;
+        // AuthenticaionFilter에서 생성된 토큰으로부터 아이디와 비밀번호를 조회함
+        String userEmail = token.getName();
+        String userPw = (String) token.getCredentials();
+        // UserDetailsService를 통해 DB에서 아이디로 사용자 조회
+        UserDetailsVO userDetailsVO = (UserDetailsVO) userDetailsService.loadUserByUsername(userEmail);
+        if (!passwordEncoder.matches(userPw, userDetailsVO.getPassword())) {
+            throw new BadCredentialsException(userDetailsVO.getUsername() + "Invalid password");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetailsVO, userPw, userDetailsVO.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+
+}
