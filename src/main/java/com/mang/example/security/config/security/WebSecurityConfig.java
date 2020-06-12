bc08c50c9f934969f243582e0d2a7ee604fd3e6f
MangKyu <whalsrb1226@naver.com>
2020-06-12 12:06:03 +0900

    Add Security configurations

--- src/main/java/com/mang/example/security/config/security/WebSecurityConfig.java
+++ src/main/java/com/mang/example/security/config/security/WebSecurityConfig.java
@@ -1,13 +1,16 @@
 package com.mang.example.security.config.security;
 
+import com.mang.example.security.handler.CustomLoginSuccessHandler;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 // /about 요청에 대해서는 로그인을 요구함
                 .antMatchers("/about").authenticated()
                 // /admin 요청에 대해서는 ROLE_ADMIN 역할을 가지고 있어야 함
@@ -30,55 +33,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 // 로그인하는 경우에 대해 설정함
-                .formLogin()
+            .formLogin()
                 // 로그인 페이지를 제공하는 URL을 설정함
                 .loginPage("/user/loginView")
-                // 로그인을 처리하는 URL을 설정함
-                .loginProcessingUrl("/user/login")
                 // 로그인 성공 URL을 설정함
                 .successForwardUrl("/index")
                 // 로그인 실패 URL을 설정함
                 .failureForwardUrl("/index")
-                // 로그인은 모든 사용자가 접근가능하도록 함
-                .permitAll();
+                .permitAll()
+                .and()
+                .addFilterBefore(customAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
-    public BCryptPasswordEncoder createBCryptPasswordEncoder() {
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    /*
-    @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) {
-        authenticationManagerBuilder.authenticationProvider(createCustomAuthenticationProvider());
+    @Bean
+    public CustomAuthenticationFilter customAuthenticationFilter() throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManager());
+        customAuthenticationFilter.setFilterProcessesUrl("/user/login");
+        customAuthenticationFilter.setAuthenticationSuccessHandler(customLoginSuccessHandler());
+        customAuthenticationFilter.afterPropertiesSet();
+        return customAuthenticationFilter;
     }
 
     @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
-        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager());
-        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
-        jwtAuthenticationFilter.setUsernameParameter("phone");
-        jwtAuthenticationFilter.setPasswordParameter("pw");
-        jwtAuthenticationFilter.setAuthenticationSuccessHandler(createCustomLoginSuccessHandler());
-        jwtAuthenticationFilter.afterPropertiesSet();
-        return jwtAuthenticationFilter;
-    }
-
-    @Bean
-    public JwtAuthorizationFilter jwtAuthorizationFilter() throws Exception {
-        return new JwtAuthorizationFilter(authenticationManager());
-    }
-
-    @Bean
-    public CustomAuthenticationProvider createCustomAuthenticationProvider() {
-        return new CustomAuthenticationProvider(createBCryptPasswordEncoder());
-    }
-
-    @Bean
-    public CustomLoginSuccessHandler createCustomLoginSuccessHandler() {
+    public CustomLoginSuccessHandler customLoginSuccessHandler() {
         return new CustomLoginSuccessHandler();
     }
-    */
+
+    @Bean
+    public CustomAuthenticationProvider customAuthenticationProvider() {
+        return new CustomAuthenticationProvider(bCryptPasswordEncoder());
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder.authenticationProvider(customAuthenticationProvider());
+    }
 
 }
