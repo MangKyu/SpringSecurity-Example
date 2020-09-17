32786cf4594bdd934963dedbf82947436b1e05a5
MangKyu <whalsrb1226@naver.com>
2020-09-18 03:01:34 +0900

    Add JwtTokenInterceptor and applied

---
+++ back/src/main/java/com/mang/example/security/config/JwtTokenInterceptor.java
@@ -0,0 +1,33 @@
+package com.mang.example.security.config;
+
+import com.mang.example.security.constants.AuthConstants;
+import com.mang.example.security.utils.TokenUtils;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Log4j2
+public class JwtTokenInterceptor implements HandlerInterceptor {
+
+    @Resource(name = "tokenUtils")
+    private TokenUtils tokenUtils;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        String header = request.getHeader(AuthConstants.AUTH_HEADER);
+
+        if (header != null) {
+            String token = tokenUtils.getTokenFromHeader(header);
+            if (tokenUtils.isValidToken(token)) {
+                return true;
+            }
+        }
+        response.sendRedirect("/error/unauthorized");
+        return false;
+    }
+
+}
---
+++ back/src/main/java/com/mang/example/security/config/web/WebMvcConfig.java
@@ -0,0 +1,38 @@
+package com.mang.example.security.config.web;
+
+import com.mang.example.security.config.JwtTokenInterceptor;
+import com.mang.example.security.config.filter.HeaderFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtTokenInterceptor())
+                .addPathPatterns("/user/findAll");
+    }
+
+    @Bean
+    public FilterRegistrationBean<HeaderFilter> getFilterRegistrationBean() {
+        FilterRegistrationBean<HeaderFilter> registrationBean = new FilterRegistrationBean<>(createHeaderFilter());
+        registrationBean.setOrder(Integer.MIN_VALUE);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+
+    @Bean
+    public HeaderFilter createHeaderFilter() {
+        return new HeaderFilter();
+    }
+
+    @Bean
+    public JwtTokenInterceptor jwtTokenInterceptor() {
+        return new JwtTokenInterceptor();
+    }
+
+}
