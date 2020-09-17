43b8fbb5ee5e17eabe08b69171f0a42cb0104b51
MangKyu <whalsrb1226@naver.com>
2020-09-18 03:03:36 +0900

    Add ErrorController

---
+++ back/src/main/java/com/mang/example/security/app/error/controller/ErrorController.java
@@ -0,0 +1,20 @@
+package com.mang.example.security.app.error.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/error")
+@Log4j2
+public class ErrorController {
+
+    @GetMapping(value = "/unauthorized")
+    public ResponseEntity unauthorized() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+}
