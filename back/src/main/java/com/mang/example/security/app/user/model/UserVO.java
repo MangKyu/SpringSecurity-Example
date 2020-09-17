21673f16629a99b50c6e32f24304d3ac1a1e4cfc
MangKyu <whalsrb1226@naver.com>
2020-09-18 03:02:42 +0900

    Refactor UserVO

--- back/src/main/java/com/mang/example/security/app/user/model/UserVO.java
+++ back/src/main/java/com/mang/example/security/app/user/model/UserVO.java
@@ -7,13 +7,11 @@ import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 
-@NoArgsConstructor
 @Entity
 @Table(name = "USER")
 @Getter
 public class UserVO extends CommonVO implements Serializable {
 
-    @Setter
     @Column(nullable = false, unique = true, length = 50)
     private String userEmail;
 
@@ -25,11 +23,5 @@ public class UserVO extends CommonVO implements Serializable {
     @Column(nullable = false, length = 50)
     @Enumerated(EnumType.STRING)
     private UserRole role;
-
-    @Builder
-    public UserVO(String userEmail, String userPw){
-        this.userEmail = userEmail;
-        this.userPw = userPw;
-    }
 
 }
