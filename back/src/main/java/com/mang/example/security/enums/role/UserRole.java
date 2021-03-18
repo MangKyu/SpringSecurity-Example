336aacb77c2e537c18f87441457acad8447aff8e
조민규 <whalsrb1226@naver.com>
2021-03-18 17:34:03 +0900

    Change to Constructor Injection with final keyword

--- back/src/main/java/com/mang/example/security/enums/role/UserRole.java
+++ back/src/main/java/com/mang/example/security/enums/role/UserRole.java
@@ -2,14 +2,15 @@ package com.mang.example.security.enums.role;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public enum UserRole {
 
     ROLE_USER("ROLE_USER"),
     ROLE_ADMIN("ROLE_ADMIN");
 
-    private String value;
+    private final String value;
 
 }
